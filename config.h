5f67a39fa55000c4e40eee14ff5758a190437665
dongzhengkuan <dongzhengkuan@huawei.com>
2023-07-11 04:04:03 +0000

    update config.h.
    Signed-off-by: dongzhengkuan <dongzhengkuan@huawei.com>

--- config.h
+++ config.h
@@ -1,5 +1,21 @@
+/*
+ * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ */
 /* config.h.  Generated from config.h.in by configure.  */
 /* config.h.in.  Generated from configure.ac by autoheader.  */
+
+#ifndef CONFIG_H
+#define CONFIG_H
 
 /* Directory where bannertopdf finds its data files (PDF templates) */
 #define BANNERTOPDF_DATADIR "/usr/share/cups/data"
@@ -87,3 +103,5 @@
 #  define CUPS_RAND() rand()
 #  define CUPS_SRAND(v) srand(v)
 #endif /* HAVE_ARC4RANDOM */
+
+#endif /* CONFIG_H */
